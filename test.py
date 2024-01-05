deb917370c84e928a347580c1fa7b689e62831a0
zwbzwb12341234 <154394854+zwbzwb12341234@users.noreply.github.com>
2024-01-05 09:17:46 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,64 @@
+import os
+import json
+
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from classic_models.alexnet import AlexNet 
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data_transform = transforms.Compose(
+        [transforms.Resize((224, 224)),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # load image
+    img_path = "D:\\Datasets\\flower\\train\\tulips\\112428665_d8f3632f36_n.jpg"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path)
+
+    plt.switch_backend('agg')
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    # read class_indict
+    json_path = "D:\\Datasets\\flower\\class_indices.json"
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    json_file = open(json_path, "r")
+    class_indict = json.load(json_file)
+
+    # create model
+    model = AlexNet(num_classes=5).to(device)
+
+    # load model weights
+    weights_path = "D:\\Codes\\Deep-Learning-Classification-Models-Based-CNN-or-Attention-main\\results\\weights\\alexnet\\AlexNet.pth"
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path))
+
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+
+    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                                                 predict[predict_cla].numpy())
+    plt.title(print_res)
+    for i in range(len(predict)):
+        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+                                                  predict[i].numpy()))
+    plt.show()
+    plt.savefig("./AelxNet_infor.jpg")
+
+
+if __name__ == '__main__':
+    main()
